0c77ff2c36336d5069bc5b0caa064000b2712547
Pushkaraj Kulkarni <107422421+pushkaraj2007@users.noreply.github.com>
2024-01-18 15:34:29 +0530

    Create rectangle_parameter_area.c

---
+++ rectangle_parameter_area.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+  int length, breadth, area, perimeter;
+  printf("Enter the length of rectangle\n");
+  scanf("%d", &length);
+  printf("Enter the width of rectangle\n");
+  scanf("%d", &breadth);
+  perimeter = 2*(length+breadth);
+  area = length * width;
+  printf("The perimeter of the rectangle is: %d\n",perimeter);
+  printf("The area of the rectangle is: %d\n",area);
+  return 0;
+}
